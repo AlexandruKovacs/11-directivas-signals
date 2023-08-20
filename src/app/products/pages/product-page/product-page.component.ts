ded51154a1da70bd63fc8005387f2b43e4dd3426
Alex Kovacs <axkovacs27@gmail.com>
2023-08-20 20:34:14 +0200

    commit

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent {
+
+  // constructor(private formBuilder: FormBuilder) {}
+  private formBuilder = inject( FormBuilder );
+
+  public color: string  = 'green';
+
+  public productForm: FormGroup = this.formBuilder.group({
+    name: ['', [ Validators.required, Validators.minLength(6), Validators.email ] ],
+  });
+
 
 }

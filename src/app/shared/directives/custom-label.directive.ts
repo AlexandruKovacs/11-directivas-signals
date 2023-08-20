11603eb71c84a6a57f6e3b221dc895c37474d823
Alex Kovacs <axkovacs27@gmail.com>
2023-08-20 20:55:07 +0200

    commit

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective implements OnInit {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
+
+  constructor( private el: ElementRef<HTMLElement> ) {
+    // console.log('Constructor of CustomLabelDirective');
+    console.log(el);
+    this.htmlElement = el;
+  }
+
+  ngOnInit(): void {
+    console.log('NgOnInit of CustomLabelDirective');
+  }
+
+  setStyle() {
+
+    if( !this.htmlElement ) {
+      return;
+    }
+
+    this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+}
